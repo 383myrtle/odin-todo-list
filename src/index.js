fc7af28700d64f8ec6b488ee40acadfb80d7057b
383myrtle <89387525+383myrtle@users.noreply.github.com>
2025-01-28 10:06:09 +0200

    Add evaluate functions for upcoming and completed tasks

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import "./styles.css";
 import { addTask } from "./TaskController.js";
 import { render } from "./DisplayController.js";
 
-addTask("Do dishes", "With soap", new Date(2025, 0, 27), "high");
-addTask("Feed cat", "With soap", new Date(2025, 0, 27), "high");
-addTask("Walk fish", "With soap", new Date(2025, 0, 28), "high");
+addTask("Feed cat", "With soap", new Date(2025, 0, 28), 2);
+addTask("Do dishes", "With soap", new Date(2025, 0, 29), 2);
+addTask("Walk fish", "With soap", new Date(2025, 0, 28), 2);
 
 
 const tabs = [
@@ -13,8 +13,16 @@ const tabs = [
         const today = new Date(Date.now());
         return (task.dueDate.toDateString() === today.toDateString()) ? true : false;
     }},
-    {id: "upcoming", name: "Upcoming"},
-    {id: "completed", name: "Completed"},
+    {id: "upcoming", name: "Upcoming", evaluate: (task) => {
+        const today = new Date(Date.now());
+        today.setHours(0,0,0,0);
+        const nextWeek = new Date(Date.now() + 604800000);
+        nextWeek.setHours(23,59,59,999);
+        return (task.dueDate <= nextWeek && task.dueDate >= today) ? true : false;
+    }},
+    {id: "completed", name: "Completed", evaluate: (task) => {
+        return (task.isCompleted());
+    }},
 ];
 
 tabs.forEach((mode) => {
