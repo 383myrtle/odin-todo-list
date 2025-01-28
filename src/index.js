5ccee3ba5e70b1b8885fc447e07e58a50cb698b3
383myrtle <89387525+383myrtle@users.noreply.github.com>
2025-01-28 11:23:13 +0200

    Update date evaluation logic using date-fns

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./normalize.css";
 import "./styles.css";
 import { addTask } from "./TaskController.js";
 import { render } from "./DisplayController.js";
+import { isToday, isFuture } from "date-fns";
 
 addTask("Feed cat", "With soap", new Date(2025, 0, 28), 2);
 addTask("Do dishes", "With soap", new Date(2025, 0, 29), 2);
@@ -10,15 +11,10 @@ addTask("Walk fish", "With soap", new Date(2025, 0, 28), 2);
 
 const tabs = [
     {id: "today", name: "Today", evaluate: (task) =>{
-        const today = new Date(Date.now());
-        return (task.dueDate.toDateString() === today.toDateString()) ? true : false;
+        return (isToday(task.dueDate)) ? true : false;
     }},
     {id: "upcoming", name: "Upcoming", evaluate: (task) => {
-        const today = new Date(Date.now());
-        today.setHours(0,0,0,0);
-        const nextWeek = new Date(Date.now() + 604800000);
-        nextWeek.setHours(23,59,59,999);
-        return (task.dueDate <= nextWeek && task.dueDate >= today) ? true : false;
+        return (isFuture(task.dueDate)) ? true : false;
     }},
     {id: "completed", name: "Completed", evaluate: (task) => {
         return (task.isCompleted());
@@ -28,6 +24,9 @@ const tabs = [
 tabs.forEach((mode) => {
     const tab = document.getElementById(mode.id);
     tab.addEventListener("click", () => {
+        const currentTab = document.querySelector(".active");
+        currentTab.classList.remove("active");
+        tab.classList.add("active");    
         render(mode);
     });
 });
