c74c3de61d925c5996bfff2f44340077a9ceb114
383myrtle <89387525+383myrtle@users.noreply.github.com>
2025-01-28 14:27:41 +0200

    Wrap everything in event on DOM content load

--- src/index.js
+++ src/index.js
@@ -1,32 +1,42 @@
 import "./normalize.css";
 import "./styles.css";
-import { addTask } from "./TaskController.js";
+import { addTask, loadFromLocalStorage } from "./TaskController.js";
 import { render } from "./DisplayController.js";
 import { isToday, isFuture } from "date-fns";
 
-addTask("Feed cat", "With soap", new Date(2025, 0, 28), 2);
-addTask("Do dishes", "With soap", new Date(2025, 0, 29), 2);
-addTask("Walk fish", "With soap", new Date(2025, 0, 28), 2);
+document.addEventListener("DOMContentLoaded", () => {
+    loadFromLocalStorage();
 
+    let input = prompt("Enter a task:");
+    while (input !== "q"){
+        let desc = prompt("Enter a description:");
+        addTask(input, desc, new Date(Date.now()), 1);
+        console.log("Task added successfully");
+        input = prompt("Enter a task:")
+    }
 
-const tabs = [
-    {id: "today", name: "Today", evaluate: (task) =>{
-        return (isToday(task.dueDate)) ? true : false;
-    }},
-    {id: "upcoming", name: "Upcoming", evaluate: (task) => {
-        return (isFuture(task.dueDate)) ? true : false;
-    }},
-    {id: "completed", name: "Completed", evaluate: (task) => {
-        return (task.isCompleted());
-    }},
-];
-
-tabs.forEach((mode) => {
-    const tab = document.getElementById(mode.id);
-    tab.addEventListener("click", () => {
-        const currentTab = document.querySelector(".active");
-        currentTab.classList.remove("active");
-        tab.classList.add("active");    
-        render(mode);
+    const tabs = [
+        {id: "today", name: "Today", evaluate: (task) =>{
+            return (isToday(task.dueDate)) ? true : false;
+        }},
+        {id: "upcoming", name: "Upcoming", evaluate: (task) => {
+            return (isFuture(task.dueDate)) ? true : false;
+        }},
+        {id: "completed", name: "Completed", evaluate: (task) => {
+            return (task.isCompleted());
+        }},
+    ];
+    
+    tabs.forEach((mode) => {
+        const tab = document.getElementById(mode.id);
+        tab.addEventListener("click", () => {
+            const currentTab = document.querySelector(".active");
+            currentTab.classList.remove("active");
+            tab.classList.add("active");    
+            render(mode);
+        });
     });
-});
+
+    /* Start on today tab on page load */
+    render(tabs[0]);
+})
