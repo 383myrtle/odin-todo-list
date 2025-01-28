a80c348b07bae239f2f8265e1b41dec5951d4e7c
383myrtle <89387525+383myrtle@users.noreply.github.com>
2025-01-28 09:23:25 +0200

    Add event listeners for sidebar tabs

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
-import "./styles.css";
 import "./normalize.css";
+import "./styles.css";
+import { addTask } from "./TaskController.js";
+import { render } from "./DisplayController.js";
+
+addTask("Do dishes", "With soap", new Date(2025, 0, 27), "high");
+addTask("Feed cat", "With soap", new Date(2025, 0, 27), "high");
+addTask("Walk fish", "With soap", new Date(2025, 0, 28), "high");
+
+
+const tabs = [
+    {id: "today", name: "Today", evaluate: (task) =>{
+        const today = new Date(Date.now());
+        return (task.dueDate.toDateString() === today.toDateString()) ? true : false;
+    }},
+    {id: "upcoming", name: "Upcoming"},
+    {id: "completed", name: "Completed"},
+];
+
+tabs.forEach((mode) => {
+    const tab = document.getElementById(mode.id);
+    tab.addEventListener("click", () => {
+        render(mode);
+    });
+});
