d6580f78f8ad8db0f65c682e802345cb9234915a
383myrtle <89387525+383myrtle@users.noreply.github.com>
2025-01-28 14:28:14 +0200

    Save tasks to localStorage

--- src/TaskController.js
+++ src/TaskController.js
@@ -4,10 +4,28 @@ const tasks = [];
 
 const addTask = (name, description, dueDate, priority, project="Default") => {
     tasks.push(new Task(name, description, dueDate, priority, project="Default"));
+    saveToLocalStorage();
+}
+
+const deleteTask = (index) => {
+    tasks.splice(index, 1);
+    saveToLocalStorage();
 }
 
 const getTasks = () => {
     return tasks;
 }
 
-export { getTasks, addTask };
+const saveToLocalStorage = () => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+const loadFromLocalStorage = () => {
+    const storedTasks = JSON.parse(localStorage.getItem("tasks"));
+    if (storedTasks) {
+        tasks.length = 0; // Clear array without changing reference
+        storedTasks.forEach(task => tasks.push(Object.assign(new Task(), task)));
+    }
+};
+
+export { getTasks, addTask, deleteTask, loadFromLocalStorage };
