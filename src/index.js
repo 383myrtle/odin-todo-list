808ae5cac5661079c67c274e256cefedbbc535df
383myrtle <89387525+383myrtle@users.noreply.github.com>
2025-01-29 11:59:52 +0200

    Render projects on page load

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import "./normalize.css";
 import "./styles.css";
 import { addTask, loadFromLocalStorage } from "./TaskController.js";
-import { render } from "./DisplayController.js";
+import { renderTasks, renderProjects } from "./DisplayController.js";
 import { isToday, isFuture } from "date-fns";
 
 document.addEventListener("DOMContentLoaded", () => {
     loadFromLocalStorage();
 
-    let input = prompt("Enter a task:");
+/*     let input = prompt("Enter a task:");
     while (input !== "q"){
         let desc = prompt("Enter a description:");
         addTask(input, desc, new Date(Date.now()), 1);
         console.log("Task added successfully");
         input = prompt("Enter a task:")
     }
-
+ */
     const tabs = [
         {id: "today", name: "Today", evaluate: (task) =>{
             return (isToday(task.dueDate) && !task.isCompleted()) ? true : false;
@@ -33,10 +33,11 @@ document.addEventListener("DOMContentLoaded", () => {
             const currentTab = document.querySelector(".active");
             currentTab.classList.remove("active");
             tab.classList.add("active");    
-            render(mode);
+            renderTasks(mode);
         });
     });
 
     /* Start on today tab on page load */
-    render(tabs[0]);
+    renderTasks(tabs[0]);
+    renderProjects();
 })
